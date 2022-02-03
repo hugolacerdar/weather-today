f22bd79ba56ed1e4faef238a9e79495a493d68c2
Hugo <hugolacerdar@gmail.com>
2022-02-03 18:12:14 -0300

    chore: app module and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
+import { WeatherForecastController } from './controllers/weather-forecast.controller';
+import { AuthMiddleware } from './middlewares/auth.middleware';
+import { CepValidatorMiddleware } from './middlewares/cep-validator.middleware';
+import { DateValidatorMiddleware } from './middlewares/date-validator.middleware';
+import { HttpClientsModule } from './services/http-clients/http-clients.module';
+import { DataServicesModule } from './services/data-services/data-services.module';
+import { WeatherForecastModule } from './services/use-cases/weather-forecast/weather-forecast.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [DataServicesModule, HttpClientsModule, WeatherForecastModule],
+  controllers: [WeatherForecastController],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes({
+      path: '*',
+      method: RequestMethod.ALL,
+    });
+    consumer.apply(CepValidatorMiddleware).forRoutes('get-forecast');
+    consumer.apply(DateValidatorMiddleware).forRoutes('get-forecast');
+  }
+}
